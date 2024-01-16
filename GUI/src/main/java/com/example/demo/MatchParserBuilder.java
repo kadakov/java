21e030305a02bbfdb625c4560dfd56325bf686c2
Uladzimir Kadakou <137704692+kadakov@users.noreply.github.com>
2024-01-16 10:23:48 +0300

    Delete GUI directory

--- GUI/src/main/java/com/example/demo/MatchParserBuilder.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-public abstract class MatchParserBuilder {
-
-    abstract void ParseTxt(FileSettings InputFileSettings, FileSettings OutputFileSettings) throws Exception;
-}
